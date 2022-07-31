e5a25d86c893339a6b8d10f231f8fce80849c358
korawutp <korawutp63@nu.ac.th>
2022-07-31 14:19:52 +0700

    codelab5

--- utility.js
+++ utility.js
@@ -1,12 +1,14 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; 
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjQ2NTY5LCJleHAiOjE2NTkyNTAxNjksIm5iZiI6MTY1OTI0NjU2OSwianRpIjoiV0RFVWdmV1dmNnhFNnpUeSIsInN1YiI6MTAyLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.PYFuK3h0bHY0kuCnXhoFV7fr5Ie-Rc1Z2QTBRpiUZN4"; 
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  firstname: 'Korawut', // replace with your full name.
+  lastname: 'Phuking-ngoen',
+  student_id: 63360187,
+  age: 20, // put your age.
+  gender: 'male', // replace with your gender
   department: 'CPE'
 };
 
@@ -14,14 +16,11 @@ const student = {
 exports.findStudentbyId = function (student_id, cb) {
   //-- call external api 
           request({
-            method: "POST",
-            uri: EXTERNAL_API,
+            method: "GET",
+            uri: EXTERNAL_API+student_id,
             headers: {
                 Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
+            
             }
         }).then((response) => {
             console.log('Sent');
@@ -39,6 +38,7 @@ exports.findStudentbyId = function (student_id, cb) {
             });
         });
 }
+
 
 exports.fakeStudentbyInfo = function (student_id, cb) {
 
