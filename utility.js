151eb2c7cc565ff1793885fa43e2a9f79d77ad1f
korawutp <korawutp63@nu.ac.th>
2022-08-29 20:42:01 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; 
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNjk1OTA3LCJleHAiOjE2NjE2OTk1MDcsIm5iZiI6MTY2MTY5NTkwNywianRpIjoiQTg4anVCQ0NVZEhnMFBDOSIsInN1YiI6MTAyLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.MFP6I99iVgeJ6ecgmtku6yXkXRFLGhS53hYeBA28UqM"; 
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzc4NzYxLCJleHAiOjE2NjE3ODIzNjEsIm5iZiI6MTY2MTc3ODc2MSwianRpIjoiYVVDY05kUTdsRzRWaVVsZSIsInN1YiI6MTAyLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.ehvFDbS42BVSzaPIeG2F0L-W3xpxH4j_Bps1JCUFH1g"; 
 const student = {
   firstname: 'Korawut', // replace with your full name.
   lastname: 'Phuking-ngoen',
